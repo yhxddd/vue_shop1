ab81fcfae2fa9ab8cf8fbdb43e28a2329637cada
yhxddd <872691624@qq.com>
2020-08-13 10:22:33 +0800

    电商后台管理系统

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,11 @@
-module.export = {
+module.exports = {
   chainWebpack: config => {
-    config.when(process.env.NODE_ENV === 'production',
-      config => {
-      //  产品发布的打包入口文件
-        config
-          .entry('app')
-          .clear()
-          .add('./src/main-prod.js')
-        config.set('externals', {
-          vue: 'Vue',
-          'vue-router': 'VueRouter',
-          axios: 'axios',
-          lodash: '_',
-          echarts: 'echarts',
-          nprogress: 'NProgress',
-          'vue-quill-editor': 'VueQuillEditor'
-        })
-      })
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+    })
 
-    config.when(process.env.NODE_ENV === 'development',
-      config => {
-      //  产品开发的打包入口文件
-        config
-          .entry('app')
-          .clear()
-          .add('./src/main.js')
-      })
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
   }
 }
