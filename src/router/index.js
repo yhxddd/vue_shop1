0483dc1b34d908c8e2e574227cc1f016acc3b291
yhxddd <872691624@qq.com>
2020-08-13 11:00:25 +0800

    电商后台管理系统

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+/*
 import login from '../components/login.vue'
 import home from '../components/home.vue'
 import welcome from '../components/welcome.vue'
@@ -13,19 +13,19 @@ import goodList from '../components/Goods/list.vue'
 import add from '../components/Goods/add.vue'
 import order from '../components/Order/order.vue'
 import report from '../components/Report/report.vue'
-
-// const login = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/login.vue')
-// const home = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/home.vue')
-// const welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/welcome.vue')
-// const users = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/User/users.vue')
-// const roles = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/Power/roles.vue')
-// const rights = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/Power/rights.vue')
-// const cates = () => import(/* webpackChunkName: "cates_params" */ '../components/Goods/cates.vue')
-// const params = () => import(/* webpackChunkName: "cates_params" */ '../components/Goods/params.vue')
-// const goodList = () => import(/* webpackChunkName: "goodList_add" */ '../components/Goods/list.vue')
-// const add = () => import(/* webpackChunkName: "goodList_add" */ '../components/Goods/add.vue')
-// const order = () => import(/* webpackChunkName: "order_report" */ '../components/Order/order.vue')
-// const report = () => import(/* webpackChunkName: "order_report" */ '../components/Report/report.vue')
+*/
+const login = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/login.vue')
+const home = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/home.vue')
+const welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/welcome.vue')
+const users = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/User/users.vue')
+const roles = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/Power/roles.vue')
+const rights = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/Power/rights.vue')
+const cates = () => import(/* webpackChunkName: "cates_params" */ '../components/Goods/cates.vue')
+const params = () => import(/* webpackChunkName: "cates_params" */ '../components/Goods/params.vue')
+const goodList = () => import(/* webpackChunkName: "goodList_add" */ '../components/Goods/list.vue')
+const add = () => import(/* webpackChunkName: "goodList_add" */ '../components/Goods/add.vue')
+const order = () => import(/* webpackChunkName: "order_report" */ '../components/Order/order.vue')
+const report = () => import(/* webpackChunkName: "order_report" */ '../components/Report/report.vue')
 
 Vue.use(VueRouter)
 
